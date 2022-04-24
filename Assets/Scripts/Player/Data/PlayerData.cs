315670fbd0c622fff07f039c404c02b5f049efaa
Xarcaneo <ras_xanias@hotmail.com>
2022-04-24 20:19:06 +0200

    Dash
    Dash implemented, dash animation should be changed in future

--- Assets/Scripts/Player/Data/PlayerData.cs
+++ Assets/Scripts/Player/Data/PlayerData.cs
@@ -13,7 +13,7 @@ public class PlayerData : ScriptableObject
     public int amountOfJumps = 1;
 
     [Header("Wall Jump State")]
-    public float wallJumpVelocityX = 10;
+    public Vector2 wallJumpVelocity = new Vector2(10, 25);
     public float wallJumpVelocityY = 25;
     public float wallJumpTime = 0.4f;
     public Vector2 wallJumpAngle = new Vector2(1, 2);
@@ -24,6 +24,16 @@ public class PlayerData : ScriptableObject
 
     [Header("Wall Slide State")]
     public float wallSlideVelocity = 3f;
+
+    [Header("Dash State")]
+    public float dashCooldown = 0.5f;
+    public float maxHoldTime = 1f;
+    public float holdTimeScale = 0.25f;
+    public float dashTime = 0.2f;
+    public float dashVelocity = 30f;
+    public float drag = 10f;
+    public float dashEndYMultiplier = 0.2f;
+    public float distBetweenAfterImages = 0.5f;
 
     [Header("Check Variables")]
     public float groundCheckRadius = 0.3f;
--- Assets/Scripts/Player/PlayerStates/SubStates/PlayerInAirState.cs
+++ Assets/Scripts/Player/PlayerStates/SubStates/PlayerInAirState.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class PlayerInAirState : PlayerState
 {
+    //Input
     private int xInput;
+    private bool jumpInput;
+    private bool jumpInputStop;
+    private bool dashInput;
+
+    //Checks
     private bool isGrounded;
     private bool isTouchingWall;
     private bool isTouchingWallBack;
     private bool oldIsTouchingWall;
     private bool oldIsTouchingWallBack;
-    private bool jumpInput;
-    private bool jumpInputStop;
+
     private bool coyoteTime;
     private bool wallJumpCoyoteTime;
     private bool isJumping;
-    private bool grabInput;
 
     private float startWallJumpCoyoteTime;
 
@@ -52,7 +56,7 @@ public class PlayerInAirState : PlayerState
         oldIsTouchingWall = false;
         oldIsTouchingWallBack = false;
         isTouchingWall = false;
-        oldIsTouchingWallBack = false;
+        isTouchingWallBack = false;
     }
 
     public override void LogicUpdate()
@@ -65,6 +69,7 @@ public class PlayerInAirState : PlayerState
         xInput = player.InputHandler.NormInputX;
         jumpInput = player.InputHandler.JumpInput;
         jumpInputStop = player.InputHandler.JumpInputStop;
+        dashInput = player.InputHandler.DashInput;
 
         CheckJumpMultiplier();
 
@@ -86,6 +91,10 @@ public class PlayerInAirState : PlayerState
         else if (isTouchingWall && xInput == player.FacingDirection && player.CurrentVelocity.y <= 0)
         {
             stateMachine.ChangeState(player.WallSlideState);
+        }
+        else if (dashInput && player.DashState.CheckIfCanDash())
+        {
+            stateMachine.ChangeState(player.DashState);
         }
         else
         {
