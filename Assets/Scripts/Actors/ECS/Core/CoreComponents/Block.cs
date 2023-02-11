daa720ef19f0704162065d25a9e8527cf3bd36d1
Xarcaneo <ras_xanias@hotmail.com>
2023-02-11 23:26:07 +0100

    Block core component

--- Assets/Scripts/Actors/ECS/Core/CoreComponents/Block.cs
+++ Assets/Scripts/Actors/ECS/Core/CoreComponents/Block.cs
@@ -7,8 +7,15 @@ public class Block : CoreComponent
     [Range(-180f, 180f)] public float MinAngle;
     [Range(-180f, 180f)] public float MaxAngle;
 
-    public bool IsBetween(float angle)
+    public bool isBlocking = false;
+
+    private Movement Movement { get => movement ?? core.GetCoreComponent(ref movement); }
+    private Movement movement;
+
+    public bool IsBetween(GameObject source)
     {
+        float angle = AngleFromFacingDirection(core.Parent.transform, source.transform, Movement.FacingDirection);
+ 
         if (MaxAngle > MinAngle)
         {
             return angle >= MinAngle && angle <= MaxAngle;
@@ -16,9 +23,9 @@ public class Block : CoreComponent
 
         return (angle >= MinAngle && angle <= 180f) || (angle <= MaxAngle && angle >= -180f);
     }
-
-    private float DetermineDamageSourceDirection(GameObject source)
+    private float AngleFromFacingDirection(Transform receiver, Transform source, int direction)
     {
-        return 0.0f; /*CombatUtilities.AngleFromFacingDirection(transform, source.transform, movement.FacingDirection);*/
+        return Vector2.SignedAngle(Vector2.right * direction,
+            source.position - receiver.position) * direction;
     }
 }
