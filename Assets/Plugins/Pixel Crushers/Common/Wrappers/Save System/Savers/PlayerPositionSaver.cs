e703d2163ee646d5aeb327c4a61dcf68bb4a0f7b
Xarcaneo <ras_xanias@hotmail.com>
2023-03-09 17:21:20 +0100

    ProCamera2D Integration

--- Assets/Plugins/Pixel Crushers/Common/Wrappers/Save System/Savers/PlayerPositionSaver.cs
+++ Assets/Plugins/Pixel Crushers/Common/Wrappers/Save System/Savers/PlayerPositionSaver.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
-using UnityEngine.SceneManagement;
 
 namespace PixelCrushers
 {
@@ -14,8 +9,6 @@ namespace PixelCrushers
             base.ApplyData(s);
             var cameraPos = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
             Camera.main.transform.position = cameraPos;
-            var vcam = FindObjectOfType<Cinemachine.CinemachineVirtualCamera>();
-            vcam.PreviousStateIsValid = false;
         }
 
     }
--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,16 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
+using Com.LuisPedroFonseca.ProCamera2D;
 using UnityEngine;
-using Cinemachine;
+
 
 public class CameraController : MonoBehaviour
 {
-    CinemachineVirtualCameraBase virtualCamera;
-    bool isCameraInitialized = false;
+    private ProCamera2D m_proCamera2D;
 
-    void Awake() =>
+    void Awake()
+    {
         // Subscribe to the player spawned event
         GameEvents.Instance.onPlayerSpawned += OnPlayerSpawned;
+
+        m_proCamera2D = GetComponent<ProCamera2D>();
+    }
 
     void OnDestroy()
     {
@@ -20,24 +22,6 @@ public class CameraController : MonoBehaviour
 
     void OnPlayerSpawned()
     {
-        if (!isCameraInitialized) // Initialize the camera only once
-        {
-            virtualCamera = GetComponent<CinemachineVirtualCameraBase>();
-
-            if (virtualCamera != null && Player.Instance != null)
-                virtualCamera.LookAt = virtualCamera.Follow = Player.Instance.transform;
-
-            isCameraInitialized = true;
-        }
-    }
-
-    void LateUpdate()
-    {
-        // Get the current camera position and round it to the nearest pixel
-        Vector3 currentPosition = transform.position;
-        Vector3 roundedPosition = new Vector3(Mathf.Round(currentPosition.x), Mathf.Round(currentPosition.y), currentPosition.z);
-
-        // Set the camera position to the rounded position
-        transform.position = roundedPosition;
+        m_proCamera2D.AddCameraTarget(Player.Instance.transform);
     }
 }
