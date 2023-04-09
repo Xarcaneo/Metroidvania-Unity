871e02eabe2ca828b019d87f4f5fffcbb61f668b
Xarcaneo <ras_xanias@hotmail.com>
2023-04-09 14:45:47 +0200

    Portal interactable object

---
+++ Assets/Scripts/Interact System/Interactables/Portal.cs
@@ -0,0 +1,31 @@
+using PixelCrushers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : Interactable
+{
+    [SerializeField] ScenePortal m_scenePortal;
+
+    public Animator Anim { get; private set; }
+    private void Start()
+    {
+        Anim = GetComponent<Animator>();
+    }
+
+    public override void Interact()
+    {
+        GameEvents.Instance.ChangingScene(true);
+        Anim.SetBool("activated", true);
+    }
+
+    public void AnimationFinished()
+    {
+        m_scenePortal.UsePortal();
+    }
+
+    public void AnimationTriggerEvent()
+    {
+        Player.Instance.gameObject.SetActive(false);
+    }
+}
