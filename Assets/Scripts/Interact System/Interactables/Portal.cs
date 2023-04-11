8e40b44910df5548e5cd978a3d57cdc918719be9
Xarcaneo <ras_xanias@hotmail.com>
2023-04-11 14:19:37 +0200

    Bugfixes

--- Assets/Scripts/Interact System/Interactables/Portal.cs
+++ Assets/Scripts/Interact System/Interactables/Portal.cs
@@ -1,3 +1,4 @@
+using Com.LuisPedroFonseca.ProCamera2D;
 using PixelCrushers;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using UnityEngine;
 public class Portal : Interactable
 {
     [SerializeField] ScenePortal m_scenePortal;
+    [SerializeField] GameObject m_cameraHook;
 
     public Animator Anim { get; private set; }
     private void Start()
@@ -27,5 +29,21 @@ public class Portal : Interactable
     public void AnimationTriggerEvent()
     {
         Player.Instance.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            FindObjectOfType<ProCamera2D>().AddCameraTarget(m_cameraHook.transform);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            FindObjectOfType<ProCamera2D>().RemoveCameraTarget(m_cameraHook.transform);
+        }
     }
 }
