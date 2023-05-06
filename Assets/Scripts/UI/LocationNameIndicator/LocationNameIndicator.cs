8bf38fcc9092667ea75f7ec436b6b8fece69ab1c
Xarcaneo <ras_xanias@hotmail.com>
2023-05-06 20:47:36 +0200

    AreaNameIndicator

---
+++ Assets/Scripts/UI/LocationNameIndicator/LocationNameIndicator.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class LocationNameIndicator : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI locationName;
+    
+    private float fadeInTime = 1f;
+    private float timeBeforeFadeOut = 2f;
+    private float fadeOutTime = 1f;
+
+    private Coroutine fadeCoroutine;
+
+    private void Awake() => GameEvents.Instance.onAreaChanged += OnAreaChanged;
+    private void OnDestroy() => GameEvents.Instance.onAreaChanged -= OnAreaChanged;
+    public void Cancelcoroutine()
+    {
+        // Cancel the current fade coroutine if it is running
+        if (fadeCoroutine != null)
+        {
+            StopCoroutine(fadeCoroutine);
+            locationName.color = new Color(locationName.color.r, locationName.color.g, locationName.color.b, 0f);
+        }
+    }
+
+    private void OnAreaChanged(string areaName)
+    {
+        if (locationName.text != areaName)
+        {
+            locationName.text = areaName;
+            fadeCoroutine = StartCoroutine(FadeInAndOut());
+        }
+    }
+    private IEnumerator FadeInAndOut()
+    {
+        float currentTime = 0f;
+        while (currentTime < fadeInTime)
+        {
+            float alpha = Mathf.Lerp(0f, 1f, currentTime / fadeInTime);
+            locationName.color = new Color(locationName.color.r, locationName.color.g, locationName.color.b, alpha);
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+
+        yield return new WaitForSecondsRealtime(timeBeforeFadeOut);
+
+        currentTime = 0f;
+        while (currentTime < fadeOutTime)
+        {
+            float alpha = Mathf.Lerp(1f, 0f, currentTime / fadeOutTime);
+            locationName.color = new Color(locationName.color.r, locationName.color.g, locationName.color.b, alpha);
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
