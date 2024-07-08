a6da1df57435b30de2da8310625fa8670e854274
Xarcaneo <ras_xanias@hotmail.com>
2024-07-08 18:16:19 +0200

    Dialogue system animation changes lua command added

--- Assets/Scripts/Utilities/CustomLua.cs
+++ Assets/Scripts/Utilities/CustomLua.cs
@@ -8,12 +8,14 @@ public class CustomLua : MonoBehaviour
     {
         Lua.RegisterFunction(nameof(CameraNewTarget), this, SymbolExtensions.GetMethodInfo(() => CameraNewTarget(string.Empty)));
         Lua.RegisterFunction(nameof(TriggerStateChanged), this, SymbolExtensions.GetMethodInfo(() => TriggerStateChanged(0)));
+        Lua.RegisterFunction(nameof(PlayAnimation), this, SymbolExtensions.GetMethodInfo(() => PlayAnimation(string.Empty, string.Empty)));
     }
 
     void OnDisable()
     {
-        Lua.UnregisterFunction(nameof(CameraNewTarget)); // <-- Only if not on Dialogue Manager.
+        Lua.UnregisterFunction(nameof(CameraNewTarget)); 
         Lua.UnregisterFunction(nameof(TriggerStateChanged));
+        Lua.UnregisterFunction(nameof(PlayAnimation));
     }
 
     private void CameraNewTarget(String cameraHookID) => GameEvents.Instance.CameraNewTarget(cameraHookID);
@@ -21,5 +23,29 @@ public class CustomLua : MonoBehaviour
     {
         DialogueLua.SetVariable("Trigger." + triggerID, true);
         GameEvents.Instance.TriggerStateChanged((int)triggerID);
+    }
+
+    public void PlayAnimation(string objectName, string animationName)
+    {
+        // Find the GameObject in the scene.
+        GameObject obj = GameObject.Find(objectName);
+        if (obj != null)
+        {
+            // Get the Animator component attached to the GameObject.
+            Animator animator = obj.GetComponent<Animator>();
+            if (animator != null)
+            {
+                // Play the specified animation.
+                animator.Play(animationName);
+            }
+            else
+            {
+                Debug.LogError("Animator component not found on the object: " + objectName);
+            }
+        }
+        else
+        {
+            Debug.LogError("Object not found: " + objectName);
+        }
     }
 }
